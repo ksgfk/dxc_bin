79a3eff80042540769ec41fba96ad64fec5e674e
ksgfk <1537100607@qq.com>
2024-08-20 21:51:03 +0800

    done

--- compile.ps1
+++ compile.ps1
@@ -21,30 +21,35 @@ $cmake_conf.Add("-DCMAKE_BUILD_TYPE=Release")
 if ($IsWindows) {
     $cmake_conf.AddRange(@("-T", "ClangCL"))
 }
-Start-Process -FilePath "cmake" -ArgumentList $cmake_conf -Wait -NoNewWindow
+$config_pros = Start-Process -FilePath "cmake" -ArgumentList $cmake_conf -Wait -NoNewWindow -PassThru
+if ($config_pros.ExitCode -ne 0) {
+    throw "cannot config"
+}
 
 $cmake_bu = @("--build", $build_dir, "--config", "Release")
-Start-Process -FilePath "cmake" -ArgumentList $cmake_bu -Wait -NoNewWindow
-
-tree $build_dir /F /A
+$compile_proc = Start-Process -FilePath "cmake" -ArgumentList $cmake_bu -Wait -NoNewWindow -PassThru
+if ($compile_proc.ExitCode -ne 0) {
+    throw "cannot config"
+}
 
 $bin_dir = "dxc_binary"
 New-Item -Path $bin_dir -ItemType Directory
 New-Item -Path "$bin_dir/bin" -ItemType Directory
-New-Item -Path "$bin_dir/lib" -ItemType Directory
 New-Item -Path "$bin_dir/include" -ItemType Directory
 Copy-Item "$dxc/include/dxc/dxcapi.h" -Destination "$bin_dir/include"
 Copy-Item "$dxc/include/dxc/dxcerrors.h" -Destination "$bin_dir/include"
 Copy-Item "$dxc/include/dxc/dxcisense.h" -Destination "$bin_dir/include"
 if ($IsLinux) {
     Copy-Item "$build_dir/bin/dxc" -Destination "$bin_dir/bin"
-    Copy-Item "$build_dir/lib/libdxcompiler.so" -Destination "$bin_dir/lib"
+    Copy-Item "$build_dir/lib/libdxcompiler.so" -Destination "$bin_dir/bin"
 } elseif ($IsMacOS) {
     Copy-Item "$build_dir/bin/dxc" -Destination "$bin_dir/bin"
-    Copy-Item "$build_dir/lib/libdxcompiler.dylib" -Destination "$bin_dir/lib"
+    Copy-Item "$build_dir/lib/libdxcompiler.dylib" -Destination "$bin_dir/bin"
 } elseif ($IsWindows) {
     Copy-Item "$build_dir/bin/dxc.exe" -Destination "$bin_dir/bin"
-    Copy-Item "$build_dir/lib/dxcompiler.dll" -Destination "$bin_dir/lib"
+    Copy-Item "$build_dir/bin/dxc.pdb" -Destination "$bin_dir/bin"
+    Copy-Item "$build_dir/bin/dxcompiler.dll" -Destination "$bin_dir/bin"
+    Copy-Item "$build_dir/bin/dxcompiler.pdb" -Destination "$bin_dir/bin"
 }
 
 $zip_name = ""
